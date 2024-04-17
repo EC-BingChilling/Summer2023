79da2b80ad49cbfb56854aa3f6ca6fd22945033e
Owen Ryan <owen.t.ryan@gmail.com>
2024-04-17 10:00:19 -0400

    Learning the logistic Equation

---
+++ testing.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+KAPPA = 0.1
+
+def logistic(lmbda):
+    return 1/(1+np.e**(lmbda*KAPPA))
+
+def main():
+    x = np.arange(-100, 100, 0.5)
+    y = np.apply_along_axis(logistic, 0, x)
+    plt.plot(x, y)
+    plt.title('Test')
+    plt.xlabel('Lamdba')
+    plt.ylabel('Logigistic')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == '__main__':
+    print(logistic(2))
+    print(logistic(-50))
+
+    main()
